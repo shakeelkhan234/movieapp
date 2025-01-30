4963dfa201e75f1361a48a7ed3372b996f39a3c3
shakeelkhan234 <shakeelkhan187004@gmail.com>
2025-01-30 13:09:15 +0500

    Update SubmitMovie.js

--- src/secondpage/SubmitMovie.js
+++ src/secondpage/SubmitMovie.js
@@ -3,13 +3,12 @@ import  './secondpage.css'
 export const SubmitMovie = () => {
   return (
     <div className='submitmovie'>
-        <h1 id='formtitle' > Submit a movie</h1>
+        <h1 id='formtitle' > Submit a note </h1>
         <form   enctype="multipart/form-data"   className='movieform' action='https://moviebackend-lo5u.vercel.app/movies' method='POST'>
             <input type='text' name="title" placeholder='title' />
-            <input type='number' name="year" placeholder='year'/>
+            
             <input type='text' name="genre" placeholder='genre'/>
-            <input type='number' name="rating" placeholder='rating'/>
-            <input type='file' name="image" placeholder='image'/>
+           >
             <button>Submit</button>
             </form>
     </div>
