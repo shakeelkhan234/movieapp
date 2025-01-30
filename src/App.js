0099e33efd3ed049aef500e05aa88ff1ff3b03b4
shakeelkhan234 <shakeelkhan187004@gmail.com>
2025-01-30 14:00:56 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,10 +18,7 @@ function App() {
   }, []);  // 
   return (
     <div>
-      // <div className='firstpage'>
-      //   <Header />
-      //   <Main />
-      // </div>
+     
 
       <div className='secondpage'>
       {movies.map((movie,index) => (
